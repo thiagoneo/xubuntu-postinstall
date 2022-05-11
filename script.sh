a67fe38292cae38ee99209ad9fd7f68d6fc283c5
Thiago Sousa <80588082+thiagoneo@users.noreply.github.com>
2022-05-10 23:08:39 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -38,12 +38,12 @@ sudo apt update
 #------------------------ INSTALAR CHROME E ANYDESK ---------------------------#
 cd $SCR_DIRECTORY/packages
 wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install ./google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb -y
 
 wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | sudo apt-key add -
 echo "deb http://deb.anydesk.com/ all main" | sudo tee /etc/apt/sources.list.d/anydesk-stable.list
 sudo apt update
-sudo apt install anydesk
+sudo apt install anydesk -y
 
 
 #---------------------- REMOVER SNAP COMPLETAMENTE ----------------------------#
