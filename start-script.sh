860ee6b80cbf2b9dd22176df47919eff23f1fead
Thiago Sousa <80588082+thiagoneo@users.noreply.github.com>
2022-06-04 17:49:28 -0300

    Update start-script.sh

--- start-script.sh
+++ start-script.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-wget https://github.com/thiagoneo/xubuntu-postinstall/archive/refs/tags/2022-05-20.tar.gz
-tar -xzvf 2022-05-20.tar.gz
-cd ubuntu-postinstall-2022-05-20/
+wget https://github.com/thiagoneo/xubuntu-postinstall/archive/refs/tags/2022-06-04.tar.gz
+tar -xzvf 2022-06-04.tar.gz
+cd ubuntu-postinstall-2022-06-04/
 chmod +x *.sh
 
 bash script.sh
