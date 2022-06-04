e385aa595763b1a7dfbdcdf5a2ee64422f808eea
Thiago Sousa <80588082+thiagoneo@users.noreply.github.com>
2022-06-04 17:48:31 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -34,21 +34,6 @@ cd $SCR_DIRECTORY/
 sudo \cp -rf $SCR_DIRECTORY/system-files/etc/apt/ /etc/
 sudo apt update
 
-
-#------------------------ INSTALAR CHROME E ANYDESK ---------------------------#
-cd $SCR_DIRECTORY/packages
-
-wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install ./google-chrome-stable_current_amd64.deb -y
-
-wget http://ftp.us.debian.org/debian/pool/main/p/pangox-compat/libpangox-1.0-0_0.0.2-5.1_amd64.deb
-sudo apt install ./libpangox-1.0-0_0.0.2-5.1_amd64.deb -y
-wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | sudo apt-key add -
-echo "deb http://deb.anydesk.com/ all main" | sudo tee /etc/apt/sources.list.d/anydesk-stable.list
-sudo apt update
-sudo apt install anydesk -y
-
-
 #---------------------- REMOVER SNAP COMPLETAMENTE ----------------------------#
 sudo snap remove firefox gnome-3-38-2004 gtk-common-themes
 sudo snap remove bare core20
