ec368b07da01533e147484ccb5f4d51b4f1674ba
Thiago Sousa <80588082+thiagoneo@users.noreply.github.com>
2022-06-16 11:43:14 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -62,7 +62,7 @@ sudo apt install ./google-chrome-stable_current_amd64.deb -y
 
 wget http://ftp.us.debian.org/debian/pool/main/p/pangox-compat/libpangox-1.0-0_0.0.2-5.1_amd64.deb
 sudo apt install ./libpangox-1.0-0_0.0.2-5.1_amd64.deb -y
-wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | sudo apt-key add -
+wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/anydesk.gpg
 echo "deb http://deb.anydesk.com/ all main" | sudo tee /etc/apt/sources.list.d/anydesk-stable.list
 sudo apt update
 sudo apt install anydesk -y
